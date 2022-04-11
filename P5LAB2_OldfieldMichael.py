82ed8168f25d2a9074b019babb6bdc6028e8ea14
oldfielm1224 <99039528+oldfielm1224@users.noreply.github.com>
2022-04-11 14:47:29 -0400

    Add files via upload
    Step Counter

---
+++ P5LAB2_OldfieldMichael.py
@@ -0,0 +1,9 @@
+def feet_to_steps(user_feet):
+    steps_walked = user_feet / 2.5
+    return int(steps_walked)
+
+if __name__ == '__main__':
+    input_feet = float(input())
+    steps_walked = feet_to_steps(input_feet)
+    print(int(steps_walked))
+    
